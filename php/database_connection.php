536a14a542324b662490fb61987e68c079802fa1
YazatS123 <77256681+YazatS123@users.noreply.github.com>
2026-02-23 18:57:07 -0600

    Change database connection parameters
    Updated database connection details for local development.

--- php/database_connection.php
+++ php/database_connection.php
@@ -1,20 +1,13 @@
 <?php
-// Database connection details
-
-
 $host = 'localhost';
-$dbname = 'prospfv0_frc_scouting';
-$username = 'prospfv0_scout_owl';
-$password = 'BlueHawks2025!';
-
-
+$dbname = 'frc_scouting';
+$username = 'root';
+$password = 'pw123456';
 
 try {
-    // Create a PDO instance to connect to the database
     $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-    // Handle connection errors
     die("Database connection failed: " . $e->getMessage());
 }
 ?>
